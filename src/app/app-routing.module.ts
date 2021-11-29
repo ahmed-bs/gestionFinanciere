f6544349cee0554f7ad409a7104947a1831bc4fe
ilhembensalha <bensalhailhem@gmail.com>
2021-11-29 21:06:21 +0100

    login and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RouterModule,Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NotfoundComponent }
 
    ];
@@ -23,6 +25,7 @@ const routes: Routes = [
    ContactComponent,
    HomeComponent,
    NotfoundComponent,
+   LoginComponent,
    RegisterComponent]
 })
 export class AppRoutingModule {
